27df011af7d2055cf091de461ea0c699b2185d51
Parul Som <somp4407@gmail.com>
2026-09-04 20:19:52 +0530

    Fix Login import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import Home from "./home";
-import Login from "./login";
+import Login from "./Login";
 import Dashboard from "./Dashboard";
 import HealthProfile from "./HealthProfile";
 import EmergencyPassport from "./EmergencyPassport";
